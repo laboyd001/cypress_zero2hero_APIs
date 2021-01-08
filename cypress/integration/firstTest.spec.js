54df98b5ca8ae00308824db0d5c5222f12ab8b74
Lesley Boyd <lesley@blackthorn.io>
2021-01-08 15:07:24 -0600

    added test to verify correct request and response

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -7,7 +7,24 @@ describe('Test with backend', () => {
         cy.loginToApplication()
     })
 
-    it('should log in', () => {
-        cy.log('Yeey we logged in!')
+    it('verify correct request and response', () => {
+
+        cy.server()
+        cy.route('POST', '**/articles').as('postArticles')
+        
+        cy.contains('New Article').click() 
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is the body of an article')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is the body of an article')
+            expect(xhr.response.body.article.description).to.equal('This is a description')
+        })
+
     })
 })
